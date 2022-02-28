d772f7de42180be5203915ca57fc687c7baac111
francesca-capuzzo <f.capuzzo9966@gmail.com>
2022-02-28 12:43:02 +0100

    update

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+function setting() {
+    fetch('./assets/settings/setting.json')
+    .then(response => response.json())
+    .then(applySettings)
+    .catch(error => console.log(error));
+}
+
+
+
+function pages() {
+   fetch('./assets/settings/pages.json')
+   .then(response => response.json())
+   .then(createPages)
+   .catch(error => console.log(error));
+}
+
+
+function applySettings(data) {
+    
+    document.title = data.title;
+    const title2 = document.getElementById('main-title');
+    const text2 = document.createTextNode(data.title);
+    title2.appendChild(text2);
+
+    const imageHeader = document.getElementById('header');
+    imageHeader.style.backgroundImage = "url(" + data.headerImage + ")";
+    imageHeader.className = 'imageHeader';
+
+    const footer = document.getElementsByClassName('link-container')[0];
+    for (const item of data.footerLinks) {
+        const textFooter = document.createTextNode(item.text);
+        const link = document.createElement('a');
+        link.setAttribute('href', item.url);
+        link.appendChild(textFooter);
+        footer.appendChild(link);
+    }
+    
+    if (data.theme === 'light') {
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = 'black';
+    }
+}
+
+
+function createPages(data) {
+    // const content = data.content;
+    const page = document.getElementById('page-content');
+    for (const item of data[0].content) {
+        console.log(data.content);
+        const tag = document.createElement(item.tag);
+        const text = document.createTextNode(item.text);
+        tag.appendChild(text);
+        page.appendChild(tag);
+
+        tag.setAttribute('src', item.url);
+        
+    }
+}
